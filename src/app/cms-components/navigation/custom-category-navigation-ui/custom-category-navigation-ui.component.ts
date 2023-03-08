ef86fe90b642ab6800ecb16917722a60f3eb293b
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-08 13:51:58 -0300

    change the custom category navigation ui logic and styles

--- src/app/cms-components/navigation/custom-category-navigation-ui/custom-category-navigation-ui.component.ts
+++ src/app/cms-components/navigation/custom-category-navigation-ui/custom-category-navigation-ui.component.ts
@@ -1,5 +1,13 @@
-import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Router } from '@angular/router';
+import { CustomBreakpointService } from '../../../common/services/custom-breakpoint.service';
+import { Observable } from 'rxjs';
 import {
   HamburgerMenuService,
   NavigationUIComponent,
@@ -9,19 +17,33 @@ import {
   selector: 'app-custom-category-navigation-ui',
   templateUrl: './custom-category-navigation-ui.component.html',
   styleUrls: ['./custom-category-navigation-ui.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class CustomCategoryNavigationUiComponent
   extends NavigationUIComponent
   implements OnInit
 {
+  childVisibility: number[] = [];
+  isMobile$: Observable<boolean> = this.customBreakpointService.isMobile();
   constructor(
     protected _router: Router,
     protected _renderer: Renderer2,
     protected _elemRef: ElementRef,
-    protected hamburgerMenuService: HamburgerMenuService
+    protected hamburgerMenuService: HamburgerMenuService,
+    protected customBreakpointService: CustomBreakpointService
   ) {
     super(_router, _renderer, _elemRef, hamburgerMenuService);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    super.ngOnInit();
+  }
+
+  toggleChildVisibility(index: number): void {
+    this.childVisibility = this.childVisibility.includes(index)
+      ? (this.childVisibility = this.childVisibility.filter((active) => {
+          return active != index;
+        }))
+      : [...this.childVisibility, index];
+  }
 }
