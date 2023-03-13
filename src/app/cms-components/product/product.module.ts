4582108c831f354a855e9bf45566943641135768
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-13 11:05:04 -0300

    create a product module to add the new custom module for products component

--- src/app/cms-components/product/product.module.ts
+++ src/app/cms-components/product/product.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ProductCarouselModule } from './product-carousel/product-carousel.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, ProductCarouselModule],
 })
-export class ProductModule { }
+export class ProductModule {}
