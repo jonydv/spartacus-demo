933240c1b2925cd4d1790d055b2d74637980dd70
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-20 16:02:04 -0300

    remove unused imports in the product carousel module

--- src/app/cms-components/product/product-carousel/product-carousel.module.ts
+++ src/app/cms-components/product/product-carousel/product-carousel.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { CustomProductCarouselComponent } from './product-carousel.component';
 import { CmsConfig, provideConfig } from '@spartacus/core';
 import { SwiperModule } from 'swiper/angular';
-import { RouterModule } from '@angular/router';
 import { MediaModule, IconModule } from '@spartacus/storefront';
 import { ProductCardModule } from '../product-card/product-card.module';
 
