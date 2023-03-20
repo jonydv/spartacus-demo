a28e64fa3fd479656f0524b38768376ce00a7960
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-20 16:03:15 -0300

    import the new module in the product module

--- src/app/cms-components/product/product.module.ts
+++ src/app/cms-components/product/product.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductCarouselModule } from './product-carousel/product-carousel.module';
+import { CustomProductListModule } from './custom-product-list/custom-product-list.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ProductCarouselModule],
+  imports: [CommonModule, ProductCarouselModule, CustomProductListModule],
 })
 export class ProductModule {}
