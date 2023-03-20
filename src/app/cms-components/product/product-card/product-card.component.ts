1cc0982770adc1b9cc8439a8784cb892e59d61ae
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-20 16:03:46 -0300

    add new validations in the product card component and customize styles to show different in the plp

--- src/app/cms-components/product/product-card/product-card.component.ts
+++ src/app/cms-components/product/product-card/product-card.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Product } from '@spartacus/core';
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProductCardComponent {
   @Input() product!: Product;
   @Input() isMobile: boolean = false;
+  @Input() fromProductList: boolean = false;
 }
--- src/app/cms-components/product/product-card/product-card.module.ts
+++ src/app/cms-components/product/product-card/product-card.module.ts
@@ -4,10 +4,17 @@ import { ProductCardComponent } from './product-card.component';
 import { RouterModule } from '@angular/router';
 import { MediaModule } from '@spartacus/storefront';
 import { UrlModule } from '@spartacus/core';
+import { ProductVariantStyleIconsModule } from '@spartacus/product/variants/root';
 
 @NgModule({
   declarations: [ProductCardComponent],
-  imports: [CommonModule, RouterModule, MediaModule, UrlModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MediaModule,
+    UrlModule,
+    ProductVariantStyleIconsModule,
+  ],
   exports: [ProductCardComponent],
 })
 export class ProductCardModule {}
