0f93a56a204610259dc41ff40a13c15a760648b3
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-03-23 17:43:40 -0300

    fix the checkout config for spartacus different with versions

--- src/app/spartacus/features/checkout/checkout-feature.module.ts
+++ src/app/spartacus/features/checkout/checkout-feature.module.ts
@@ -1,27 +1,39 @@
 import { NgModule } from '@angular/core';
-import { checkoutTranslationChunksConfig, checkoutTranslations } from "@spartacus/checkout/assets";
-import { CheckoutRootModule, CHECKOUT_FEATURE } from "@spartacus/checkout/root";
-import { CmsConfig, I18nConfig, provideConfig } from "@spartacus/core";
+import {
+  checkoutTranslationChunksConfig,
+  checkoutTranslations,
+} from '@spartacus/checkout/assets';
+import { ShippingAddressComponent } from '@spartacus/checkout/components';
+import { CheckoutRootModule, CHECKOUT_FEATURE } from '@spartacus/checkout/root';
+import { CmsConfig, I18nConfig, provideConfig } from '@spartacus/core';
+import { customCheckoutRoutingConfig } from './config/custom-checkout-routing.config';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CheckoutRootModule
-  ],
-  providers: [provideConfig(<CmsConfig>{
-    featureModules: {
-      [CHECKOUT_FEATURE]: {
-        module: () =>
-          import('@spartacus/checkout').then((m) => m.CheckoutModule),
+  imports: [CheckoutRootModule],
+  providers: [
+    provideConfig(customCheckoutRoutingConfig),
+    provideConfig(<CmsConfig>{
+      featureModules: {
+        [CHECKOUT_FEATURE]: {
+          module: () =>
+            import('@spartacus/checkout').then((m) => m.CheckoutModule),
+        },
       },
-    }
-  }),
-  provideConfig(<I18nConfig>{
-    i18n: {
-      resources: checkoutTranslations,
-      chunks: checkoutTranslationChunksConfig,
-    },
-  })
-  ]
+    }),
+    provideConfig(<CmsConfig>{
+      cmsComponents: {
+        CheckoutDeliveryAddress: {
+          component: ShippingAddressComponent,
+        },
+      },
+    }),
+    provideConfig(<I18nConfig>{
+      i18n: {
+        resources: checkoutTranslations,
+        chunks: checkoutTranslationChunksConfig,
+      },
+    }),
+  ],
 })
-export class CheckoutFeatureModule { }
+export class CheckoutFeatureModule {}
